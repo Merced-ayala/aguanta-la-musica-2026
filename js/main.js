02a5e9f94ca2bcc20e4968fa2326b6462f3594c1
Merced-ayala <mercyayala.r@gmail.com>
2026-05-09 19:08:57 -0500

    Proyecto de ingeniería II fase 3

---
+++ js/main.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Crear el contenedor del banner
+    const banner = document.createElement("div");
+    banner.id = "promo-banner";
+    
+    // Agregar contenido al banner con un enlace
+    banner.innerHTML = '<a href="https://wa.me/3142244057" target="_blank">50% de descuento en tu primera sesión de Musicoterapia🎁</a>';
+
+    // Insertar el banner después de la barra de navegación
+    const navbar = document.querySelector(".navbar"); // Asegúrate de que tu navbar tenga esta clase
+    if (navbar) {
+        navbar.insertAdjacentElement("afterend", banner);
+    } else {
+        document.body.insertBefore(banner, document.body.firstChild);
+    }
+});
